71c8a74f07f4df7b3240aa1b0a1da6c3e6ecdfb7
Aniceab <{ID}+{username}@users.noreply.github.com>
2025-04-17 11:36:07 -0300

    construcaopipeline

--- component-scripts/src/ingest/ingesta.py
+++ component-scripts/src/ingest/ingesta.py
@@ -26,17 +26,3 @@ def upload_url_to_s3(url, bucket_name, s3_key, aws_access_key, aws_secret_key):
         print("Credenciais AWS inválidas.")
     except Exception as e:
         print(f"Erro inesperado: {e}")
-
-# Exemplo de uso
-# if __name__ == "__main__":
-#     # URL do arquivo para download
-#     file_url = "https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page"
-#     local_file_path = "yellow_tripdata_sample_2023-01.parquet"
-
-#     # Configurações do S3
-#     bucket_name = "yellow-taxi-files-anice"
-#     s3_key = "ingesta_file/yellow_tripdata_sample_2023-01.parquet"
-#     aws_access_key = "AKIAQ62SYYVDQTYHY7WP"
-#     aws_secret_key = "IivboAdebexkiXsW+6BJHfPWb3WmYYUJKiLxTOHW"
-
-#     upload_url_to_s3(file_url, bucket_name, s3_key, aws_access_key, aws_secret_key)
