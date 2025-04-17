ba596294328da73fee1522aefc0df38c2d9a43f5
Aniceab <{ID}+{username}@users.noreply.github.com>
2025-04-17 12:01:46 -0300

    construcaopipeline

--- component-scripts/src/ingest/ingesta.py
+++ component-scripts/src/ingest/ingesta.py
@@ -10,12 +10,12 @@ def upload_url_to_s3(url, bucket_name, s3_key, aws_access_key, aws_secret_key):
     try:
         response = requests.get(url, stream=True)
         response.raise_for_status()
-
-        s3 = boto3.client(
-            's3',
-            aws_access_key_id=aws_access_key,
-            aws_secret_access_key=aws_secret_key
-        )
+        # contruir um metodo para pegar as keys 
+        #s3 = boto3.client(
+        #    's3',
+        #    aws_access_key_id=aws_access_key,
+        #    aws_secret_access_key=aws_secret_key
+        #)
 
         s3.upload_fileobj(response.raw, bucket_name, s3_key)
         print(f"Arquivo enviado diretamente para o S3: s3://{bucket_name}/{s3_key}")
